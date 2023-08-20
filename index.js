e7d65539bc69d7d45e6a8a7b20a83c965dcc3058
TheLastArchive <iarchive@hotmail.com>
2023-08-20 12:58:23 +0100

    Added main function and added check for access token refresh

--- index.js
+++ index.js
@@ -2,8 +2,16 @@
 const tracks_handler = require('./lib/tracks');
 const server = require('./lib/auth');
 
-server.start(5000);
-tracks_handler.get_recent_tracks()
+async function main() {
+    server.start(5000);
+    if (server.has_token_expired()) {
+        console.log("Access token has expired, refreshing...")
+        await server.refresh_token()
+        console.log("New token generated")
+    }
+    tracks_handler.get_recent_tracks()
+}
 
+main();
 
 
