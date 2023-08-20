b134a011f503d4dbfe375f59cc49b58f3f1e057a
TheLastArchive <iarchive@hotmail.com>
2023-08-20 14:09:33 +0100

    Added an hourly scheduled task

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-
+const schedule = require('node-schedule');
 const tracks_handler = require('./lib/tracks');
 const server = require('./lib/auth');
 
 async function main() {
+
     server.start(5000);
-    if (server.has_token_expired()) {
-        console.log("Access token has expired, refreshing...")
-        await server.refresh_token()
-        console.log("New token generated")
-    }
-    tracks_handler.get_recent_tracks()
+    //Scheduled task to get recent tracks every hour
+    schedule.scheduleJob({minute: 0}, async () => {
+        console.log("Executing scheduled task")
+        if (server.has_token_expired()) {
+            console.log("Access token has expired, refreshing...")
+            await server.refresh_token()
+            console.log("New token generated")
+        }
+        tracks_handler.get_recent_tracks()
+    })
 }
 
 main();
-
-
