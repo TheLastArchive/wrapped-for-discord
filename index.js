0cea9e9189037087648fe1cd584294d501774773
TheLastArchive <iarchive@hotmail.com>
2023-08-26 17:47:16 +0100

    Mass reformat using Prettier

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-const schedule = require('node-schedule');
-const tracks_handler = require('./lib/tracks');
-const server = require('./lib/auth');
-const db_access = require('./lib/database/data_access');
-const db_insert = require('./lib/database/data_insertion');
-
+const schedule = require("node-schedule");
+const tracks_handler = require("./lib/tracks");
+const server = require("./lib/auth");
+const db_access = require("./lib/database/data_access");
+const db_insert = require("./lib/database/data_insertion");
 
 async function main() {
-
     server.start(5000);
 
     //Scheduled task to get recent tracks every hour
-    schedule.scheduleJob({minute: 0}, update_all_users_tracks)
-
+    schedule.scheduleJob({ minute: 0 }, update_all_users_tracks);
 }
-
 
 async function update_all_users_tracks() {
     let users = await db_access.get_all_users();
@@ -22,9 +18,17 @@ async function update_all_users_tracks() {
         let user_json = user.toJSON();
         let token_json = token.toJSON();
         if (has_token_expired(token_json.token_expires_epoch)) {
-            console.log(user_json.spotify_id + "'s access token has expired, refreshing...")
-            const updated_refresh_token = await server.refresh_token(token_json);
-            await db_insert.insert_refreshed_token(token, updated_refresh_token);
+            console.log(
+                user_json.spotify_id +
+                    "'s access token has expired, refreshing..."
+            );
+            const updated_refresh_token = await server.refresh_token(
+                token_json
+            );
+            await db_insert.insert_refreshed_token(
+                token,
+                updated_refresh_token
+            );
             token_json = updated_refresh_token;
             console.log("Token refreshed and stored in the database");
         }
@@ -32,9 +36,8 @@ async function update_all_users_tracks() {
     });
 }
 
-
 function has_token_expired(token_expires_epoch) {
-    return new Date > token_expires_epoch 
+    return new Date() > token_expires_epoch;
 }
 
 main();
