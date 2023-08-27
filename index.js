16c29b0975a153dabf9d037aabb79d0f231e6bb9
TheLastArchive <iarchive@hotmail.com>
2023-08-27 13:08:46 +0100

    Refactor and handle token decrypt

--- index.js
+++ index.js
@@ -7,30 +7,20 @@ const db_insert = require("./lib/database/data_insertion");
 async function main() {
     server.start(5000);
 
-    //Scheduled task to get recent tracks every hour
     schedule.scheduleJob({ minute: 0 }, update_all_users_tracks);
 }
 
 async function update_all_users_tracks() {
     let users = await db_access.get_all_users();
     users.forEach(async (user) => {
-        let token = await user.getToken();
+        let encrypted_token = await user.getToken();
+        let token = db_access.handle_token_decrypt(encrypted_token);
         let user_json = user.toJSON();
         let token_json = token.toJSON();
         if (has_token_expired(token_json.token_expires_epoch)) {
-            console.log(
-                user_json.spotify_id +
-                    "'s access token has expired, refreshing..."
-            );
-            const updated_refresh_token = await server.refresh_token(
-                token_json
-            );
-            await db_insert.insert_refreshed_token(
-                token,
-                updated_refresh_token
-            );
-            token_json = updated_refresh_token;
-            console.log("Token refreshed and stored in the database");
+            console.log("Token expired, refreshing...");
+            token_json = await handle_token_refresh(token);
+            console.log("Token refreshed and stored in database");
         }
         tracks_handler.get_recent_tracks(token_json.access_token);
     });
@@ -40,5 +30,13 @@ function has_token_expired(token_expires_epoch) {
     return new Date() > token_expires_epoch;
 }
 
-main();
-// update_all_users_tracks();
+async function handle_token_refresh(token) {
+    let token_json = token.toJSON();
+    const updated_refresh_token = await server.refresh_token(token_json);
+    await db_insert.insert_refreshed_token(token, updated_refresh_token);
+
+    return updated_refresh_token;
+}
+
+// main();
+update_all_users_tracks();
