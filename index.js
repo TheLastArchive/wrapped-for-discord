145419cf057f64ddfd3faedae92679a88a1d059b
TheLastArchive <iarchive@hotmail.com>
2023-09-11 21:06:10 +0100

    Docstrings and refactor

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const tracks_handler = require("./lib/tracks");
 const server = require("./lib/auth");
 const db_access = require("./lib/database/data_access");
 const db_insert = require("./lib/database/data_insertion");
+const wrapped = require("./lib/wrapped");
 
 async function main() {
-    server.start(5000);
-    update_all_users_tracks();
+    // server.start(5000);
+    // await update_all_users_tracks();
+    const wrapped_object = await wrapped.handle_wrapped();
+    console.log("Gamer Aquarium");
+    console.log(wrapped_object.global_stats.total_count + " plays");
+    console.log(milliseconds_to_hours_and_minutes(wrapped_object.global_stats.total_time_listened));
+    for (const user of wrapped_object.user_stats) {
+
+        console.log(user.display_name);
+        console.log(user.total_count + " plays");
+        console.log(milliseconds_to_hours_and_minutes(user.total_time_listened));
+    }
     let hourly_task = schedule.scheduleJob(
         { minute: 0 },
         update_all_users_tracks
@@ -19,6 +30,7 @@ async function update_all_users_tracks() {
     let users = await db_access.get_all_users();
     for (const user of users) {
         console.log(user.display_name);
+
         let token = await get_token(user);
         const recent_tracks = await tracks_handler.get_recent_tracks(
             token.access_token,
@@ -61,5 +73,12 @@ async function handle_token_refresh(token) {
     return token;
 }
 
+function milliseconds_to_hours_and_minutes(milliseconds) {
+    // Calculate hours, minutes, and seconds
+    const hours = Math.floor(milliseconds / (1000 * 60 * 60));
+    const minutes = Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60));
+    return `${hours} hours and ${minutes} minutes`
+
+}
 main();
 // update_all_users_tracks();
