137a54072de4c7b7416c4b6e8769730d0665d2b7
TheLastArchive <iarchive@hotmail.com>
2023-09-12 13:54:06 +0100

    Restructured and moved methods

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const schedule = require("node-schedule");
 const tracks_handler = require("./lib/tracks");
-const server = require("./lib/auth");
+const server = require("./lib/token");
 const db_access = require("./lib/database/data_access");
 const db_insert = require("./lib/database/data_insertion");
 const wrapped = require("./lib/wrapped");
 
 async function main() {
-    // server.start(5000);
-    // await update_all_users_tracks();
-    const wrapped_object = await wrapped.handle_wrapped();
-    console.log("Gamer Aquarium");
-    console.log(wrapped_object.global_stats.total_count + " plays");
-    console.log(milliseconds_to_hours_and_minutes(wrapped_object.global_stats.total_time_listened));
-    for (const user of wrapped_object.user_stats) {
+    server.start(5000);
+    await update_all_users_tracks();
+    // const wrapped_object = await wrapped.handle_wrapped();
+    // console.log("Gamer Aquarium");
+    // console.log(wrapped_object.global_stats.total_count + " plays");
+    // console.log(milliseconds_to_hours_and_minutes(wrapped_object.global_stats.total_time_listened));
+    // for (const user of wrapped_object.user_stats) {
 
-        console.log(user.display_name);
-        console.log(user.total_count + " plays");
-        console.log(milliseconds_to_hours_and_minutes(user.total_time_listened));
-    }
+    //     console.log(user.display_name);
+    //     console.log(user.total_count + " plays");
+    //     console.log(milliseconds_to_hours_and_minutes(user.total_time_listened));
+    // }
     let hourly_task = schedule.scheduleJob(
         { minute: 0 },
         update_all_users_tracks
@@ -31,7 +31,7 @@ async function update_all_users_tracks() {
     for (const user of users) {
         console.log(user.display_name);
 
-        let token = await get_token(user);
+        let token = await server.get_token(user);
         const recent_tracks = await tracks_handler.get_recent_tracks(
             token.access_token,
             user.played_at
@@ -44,33 +44,6 @@ async function update_all_users_tracks() {
     }
     console.log("Scheduled task 'update_all_user_tracks' completed. " +
             total + " total records added");
-}
-
-function has_token_expired(token_expires_epoch) {
-    return new Date() > token_expires_epoch;
-}
-
-async function get_token(user) {
-    let encrypted_token = await user.getToken();
-    let token = db_access.handle_token_decrypt(encrypted_token);
-    if (has_token_expired(token.token_expires_epoch)) {
-        return await handle_token_refresh(token);
-    }
-    return token;
-}
-
-/**
- * @description Fetches a new refresh token, encrypts and stores in the database, updates 'token'
- * @param {*} token sequelize token for the user
- * @returns user token with the refreshed access token
- */
-async function handle_token_refresh(token) {
-    console.log("Token expired, refreshing...");
-    const refreshed_token = await server.refresh_token(token);
-    token = await db_insert.insert_refreshed_token(token, refreshed_token);
-    token.access_token = refreshed_token.access_token;
-    console.log("Token refreshed and stored in database");
-    return token;
 }
 
 function milliseconds_to_hours_and_minutes(milliseconds) {
